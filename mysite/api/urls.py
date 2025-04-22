19a6fa0fddb2f07bfa1e4c26759a56c02b61d4c4
Durdun Andrei George <147340783+andreidurdun@users.noreply.github.com>
2025-04-22 15:52:56 +0300

    Update urls.py

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path("userprofile/", views.UserListCreate.as_view(), name="userprofile-view-create"),
     path("books/", views.BookListCreate.as_view(), name="book-list-create"),
     path("libraries/", views.LibraryListCreate.as_view(), name="library-list-create"),
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
-    
 ]
