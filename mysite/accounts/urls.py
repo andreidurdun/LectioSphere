acd07397f290e74a2bad6189d9acbb8327188f93
Durdun Andrei George <147340783+andreidurdun@users.noreply.github.com>
2025-04-27 17:54:04 +0300

    Automat account activation

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from .views import UserProfileView
+from .views import ActivateAccountView
 
 urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('activate/<str:uid>/<str:token>/', ActivateAccountView.as_view(), name='activate-account'),
 ]
