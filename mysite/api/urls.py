1a3c785476308acc588180fb3f9398cca6ab83e3
Rusu Ana-Maria <ana-maria.rusu4@s.unibuc.ro>
2025-05-01 13:57:18 +0300

    Update urls.py

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -5,13 +5,13 @@ from .views import BooksView
 
 urlpatterns = [
    
-    path("books/", views.BookListCreate.as_view(), name="book-list-create"),
-    path("comments/", views.CommentListCreate.as_view(), name="comment-list-create"),
-    path("events/", views.EventListCreate.as_view(), name="event-list-create"),
-    path("posts/", views.PostListCreate.as_view(), name="post-list-create"),
-    path("reading_sheets/", views.ReadingSheetListCreate.as_view(), name="reading-sheet-list-create"),
-    path("shelves/", views.ShelfListCreate.as_view(), name="shelf-list-create"),
-    path("shelves_books/", views.ShelfBooksListCreate.as_view(), name="shelf-books-list-create"),
+    # path("books/", views.BookListCreate.as_view(), name="book-list-create"),
+    # path("comments/", views.CommentListCreate.as_view(), name="comment-list-create"),
+    # path("events/", views.EventListCreate.as_view(), name="event-list-create"),
+    # path("posts/", views.PostListCreate.as_view(), name="post-list-create"),
+    # path("reading_sheets/", views.ReadingSheetListCreate.as_view(), name="reading-sheet-list-create"),
+    # path("shelves/", views.ShelfListCreate.as_view(), name="shelf-list-create"),
+    # path("shelves_books/", views.ShelfBooksListCreate.as_view(), name="shelf-books-list-create"),
     #path("userprofile/", views.UserListCreate.as_view(), name="userprofile-view-create"),
     path("books/search/", GoogleBooksAPIView.as_view({"get": "search"}), name="google-books-search"),
     path("books/category/", GoogleBooksAPIView.as_view({"get": "category"}), name="google-books-category"),
