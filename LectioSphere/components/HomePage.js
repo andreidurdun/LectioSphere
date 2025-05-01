3dfd8440984bd2821ca0451a6e1ec9062c414273
andreeadumitru20 <andreeadumitru.alex20@gmail.com>
2025-05-01 22:24:37 +0300

    HomePage in lipsa cartilor

--- LectioSphere/components/HomePage.js
+++ LectioSphere/components/HomePage.js
@@ -1,42 +1,132 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Alert, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Alert, SafeAreaView, TouchableNativeFeedback } from 'react-native';
 import NavBar from './Partials/NavBar';
 import TopBar from './Partials/TopBar';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import { useFonts, Nunito_400Regular, Nunito_500Medium, Nunito_600SemiBold } from '@expo-google-fonts/nunito';
+import { refreshAccessToken } from './refreshAccessToken'; 
 
 const HomePage = ({ navigation, removeAuthToken, isAuthenticated, apiBaseUrl }) => {
     const [userData, setUserData] = useState(null);
+    const [continueReadingItems, setContinueReadingItems] = useState([]);
+    const [isReading, setIsReading] = useState(true);
+    const [youMayLikeItems, setYouMayLikeItems] = useState([]);
+    const [Read, setRead] = useState(true);
+    const [friendsAreReadingItems, setFriendsAreReadingItems] = useState([]);
+    const [areReading, setAreReading] = useState(true);
 
-    useEffect(() => {
-        // Verificăm dacă utilizatorul este autentificat și încărcăm datele profilului
-        if (isAuthenticated) {
-            fetchUserData();
-        }
-    }, [isAuthenticated]);
+
+    const [fontsLoaded] = useFonts({
+        Nunito_400Regular,
+        Nunito_500Medium,
+        Nunito_600SemiBold
+    });
 
     const fetchUserData = async () => {
         try {
-            const token = await AsyncStorage.getItem('auth_token');
-            if (!token) {
-                console.log('No token found');
-                return;
-            }
-
+            let token = await AsyncStorage.getItem('auth_token');
             const response = await axios.get(`${apiBaseUrl}/auth/users/me/`, {
-                headers: {
-                    'Authorization': `JWT ${token}`
-                }
+                headers: { Authorization: `JWT ${token}` }
             });
-
-            if (response.status === 200) {
-                setUserData(response.data);
-            }
+            setUserData(response.data);
         } catch (error) {
-            console.error('Error fetching user data:', error);
-            if (error.response && error.response.status === 401) {
-                // Token expirat sau invalid - delogăm utilizatorul
-                handleLogout();
+            if (error.response?.status === 401) {
+                const newToken = await refreshAccessToken(apiBaseUrl);
+                if (newToken) {
+                    const retryResponse = await axios.get(`${apiBaseUrl}/auth/users/me/`, {
+                        headers: { Authorization: `JWT ${newToken}` }
+                    });
+                    setUserData(retryResponse.data);
+                } else {
+                    handleLogout();
+                }
+            } else {
+                console.error("User fetch error:", error.message);
+            }
+        }
+    };
+
+    const fetchContinueReading = async () => {
+        try {
+            let token = await AsyncStorage.getItem('auth_token');
+            const response = await axios.get(`${apiBaseUrl}/api/books/read/get/`, {
+                headers: { Authorization: `JWT ${token}` }
+            });
+            setContinueReadingItems(response.data);
+        } catch (error) {
+            if (error.response?.status === 401) {
+                const newToken = await refreshAccessToken(apiBaseUrl);
+                if (newToken) {
+                    const retryResponse = await axios.get(`${apiBaseUrl}/api/books/read/get/`, {
+                        headers: { Authorization: `JWT ${newToken}` }
+                    });
+                    setContinueReadingItems(retryResponse.data);
+                } else {
+                    console.error("Unable to refresh for reading items.");
+                }
+            } 
+            else if (error.response?.status === 404) {
+                setIsReading(false);
+            }
+            else {
+                console.error("Continue Reading error:", error.message);
+            }
+        }
+    };
+
+    const fetchYouMayLike = async () => {
+        try {
+            let token = await AsyncStorage.getItem('auth_token');
+            const response = await axios.get(`${apiBaseUrl}/api/books/recommendation/`, {
+                headers: { Authorization: `JWT ${token}` }
+            });
+            setYouMayLikeItems(response.data);
+        } catch (error) {
+            if (error.response?.status === 401) {
+                const newToken = await refreshAccessToken(apiBaseUrl);
+                if (newToken) {
+                    const retryResponse = await axios.get(`${apiBaseUrl}/api/books/recommendation/`, {
+                        headers: { Authorization: `JWT ${newToken}` }
+                    });
+                    setYouMayLikeItems(retryResponse.data);
+                } else {
+                    console.error("Unable to refresh for reading items.");
+                }
+            } 
+            else if (error.response?.status === 404) {
+                setRead(false);
+            }
+            else {
+                console.error("You May Like error:", error.message);
+            }
+        }
+    };
+
+    const fetchFriendsAreReading = async () => {
+        try {
+            let token = await AsyncStorage.getItem('auth_token');
+            const response = await axios.get(`${apiBaseUrl}/demodificat`, {
+                headers: { Authorization: `JWT ${token}` }
+            });
+            setFriendsAreReadingItems(response.data);
+        } catch (error) {
+            if (error.response?.status === 401) {
+                const newToken = await refreshAccessToken(apiBaseUrl);
+                if (newToken) {
+                    const retryResponse = await axios.get(`${apiBaseUrl}/demodificat`, {
+                        headers: { Authorization: `JWT ${newToken}` }
+                    });
+                    setFriendsAreReadingItems(retryResponse.data);
+                } else {
+                    console.error("Unable to refresh for reading items.");
+                }
+            } 
+            else if (error.response?.status === 404) {
+                setAreReading(false);
+            }
+            else {
+                console.error("Friends are reading error:", error.message);
             }
         }
     };
@@ -47,12 +137,10 @@ const HomePage = ({ navigation, removeAuthToken, isAuthenticated, apiBaseUrl }) 
             'Are you sure you want to logout?',
             [
                 { text: 'Cancel', style: 'cancel' },
-                { 
-                    text: 'Logout', 
+                {
+                    text: 'Logout',
                     onPress: async () => {
-                        // Utilizează funcția removeAuthToken pasată din App.js
                         await removeAuthToken();
-                        // Navigăm către ecranul de login
                         navigation.replace('LoginMenu');
                     }
                 }
@@ -60,24 +148,71 @@ const HomePage = ({ navigation, removeAuthToken, isAuthenticated, apiBaseUrl }) 
         );
     };
 
+    useEffect(() => {
+        if (isAuthenticated) {
+            fetchUserData();
+            fetchContinueReading();
+            fetchYouMayLike();
+            fetchFriendsAreReading();
+        }
+    }, [isAuthenticated]);
+
+    if (!fontsLoaded) {
+        return <Text>Loading fonts...</Text>;
+    }
+
+    
+
     return (
         <SafeAreaView style={styles.screen}>
-            
-            <TopBar pageName="SearchPage" />
+            <TopBar pageName="HomePage" />
 
             <View style={styles.header}>
-                <Text style={styles.title}>Welcome to LectioSphere</Text>
-                {userData && (
-                    <Text style={styles.welcomeText}>
-                        Hello, {userData.first_name} {userData.last_name}!
-                    </Text>
-                )}
-                <Text style={styles.title}>In tot view-ul asta implementezi logica pentru HomePage</Text>
+                {/* <TouchableNativeFeedback onPress={() => console.log("Apasat")}>
+                    <View style={styles.container}>
+                        <Text style={styles.textContainer}>Continue Reading</Text>
+                        {continueReadingItems.map((item, index) => (
+                            <Text key={index}>{item.titlu}</Text>
+                        ))}
+                    </View>
+                </TouchableNativeFeedback> */}
+
+                {
+                    isReading ? (
+                        <TouchableNativeFeedback onPress={() => console.log("Apasat")}>
+                            <View style={styles.container}>
+                                <Text style={styles.textContainer}> Continue Reading</Text>
+                                {continueReadingItems.map((item, index) => (
+                                    <Text key={index}>{item.titlu}</Text>
+                                ))}
+                            </View>
+                        </TouchableNativeFeedback>
+                    ) : null 
+                }
+
+                {
+                    Read ? (
+                        <TouchableNativeFeedback onPress={() => console.log("Apasat")}>
+                            <View style={styles.container}>
+                                <Text style={styles.textContainer}> You May Like</Text>
+                            </View>
+                        </TouchableNativeFeedback>
+                    ) : null 
+                }
+
+                {
+                    areReading ? (
+                        <TouchableNativeFeedback onPress={() => console.log("Apasat")}>
+                            <View style={styles.container}>
+                                <Text style={styles.textContainer}> Your friends are reading</Text>
+                            </View>
+                        </TouchableNativeFeedback>
+                    ) : null
+                }
+
             </View>
 
-            
             <NavBar />
-
         </SafeAreaView>
     );
 };
@@ -85,55 +220,31 @@ const HomePage = ({ navigation, removeAuthToken, isAuthenticated, apiBaseUrl }) 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         padding: 0,
     },
     header: {
         width: '100%',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         marginBottom: 20,
+        marginTop: 84,
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 10,
-    },
-    welcomeText: {
-        fontSize: 18,
-        textAlign: 'center',
-        marginBottom: 10,
-        color: '#613F75',
-    },
-    subtitle: {
-        fontSize: 16,
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    button: {
+    container: {
+        backgroundColor: '#F7EDF1',
+        borderColor: '#F3E3E9',
+        borderWidth: 1,
+        borderRadius: 8,
         paddingVertical: 10,
-        paddingHorizontal: 20,
-        backgroundColor: '#613F75',
-        borderRadius: 5,
-        marginBottom: 60, // Pentru a lăsa spațiu pentru NavBar
+        width: '95%',
+        marginTop: 16,
     },
-    buttonText: {
-        fontSize: 16,
-        color: '#fff',
-        textAlign: 'center',
-    },
-    logoutButton: {
-        paddingVertical: 8,
-        paddingHorizontal: 15,
-        backgroundColor: '#f44336',
-        borderRadius: 4,
-        marginTop: 10,
-    },
-    logoutButtonText: {
-        fontSize: 14,
-        color: '#fff',
-        textAlign: 'center',
+    textContainer: {
+        fontSize: 20,
+        fontWeight: '500',
+        color: '#18101D',
+        fontFamily: 'Nunito_600SemiBold',
     },
 });
 
