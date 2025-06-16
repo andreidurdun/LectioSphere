0889de2ab58396abd64b702819339fa0905d5a69
DanielNedelcu <ionut-daniel.nedelcu@s.unibuc.ro>
2025-06-16 14:48:06 +0300

    readme final without demo

--- LectioSphere/App.js
+++ LectioSphere/App.js
@@ -27,7 +27,7 @@ import PostPartial from './components/Partials/PostPartial';
 
 const Stack = createNativeStackNavigator();
 // URL-ul de bază al serverului, utilizat în întreaga aplicație
-const API_BASE_URL = 'http://192.168.1.134:8000';
+const API_BASE_URL = 'http://192.168.199.240:8000';
 
 // Configurare interceptor global pentru axios
 const setupAxiosInterceptors = (refresh) => {
